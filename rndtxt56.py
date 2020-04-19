4a56a45d5be5dca735ef293b9b9b93e6ff8f6b9f
pachenya <57573246+pachenya@users.noreply.github.com>
2020-04-19 18:16:58 +0900

    Add files via upload

---
+++ rndtxt56.py
@@ -0,0 +1,63 @@
+# filereadng.py
+import random
+import string
+
+RNDSTRNUM = 1000
+
+def OpenTextList(filename):
+  f = open(filename)
+  lns = f.readlines()
+  f.close()
+  return lns
+
+def getrandstr(strline):
+  cnt = 0
+  rval = 'LINE'
+  for s in strline:
+    if random.randint(0, cnt) == 0:
+      rval = s.replace('\n','')
+    cnt += 1
+  return rval
+
+def buildrandstr(cnt = 32):
+  lakira = OpenTextList('a1.txt')
+  lwho   = OpenTextList('a2.txt')
+  lplace = OpenTextList('a3.txt')
+  ldo    = OpenTextList('a4.txt')
+  lobj   = OpenTextList('a5.txt')
+  lfure  = OpenTextList('a6.txt')
+  lMeta  = OpenTextList('aMeta.txt')
+  lst = []
+  #proguresubaa
+  prog = 0
+  prog = cnt // 20
+  if prog <= 0:
+    prog = 1
+  for i in range(cnt):
+    akira = getrandstr(lakira)
+    who   = getrandstr(lwho)
+    place = getrandstr(lplace)
+    do    = getrandstr(ldo)
+    obj   = getrandstr(lobj)
+    fure  = getrandstr(lfure)
+    strMeta = getrandstr(lMeta)
+    m = strMeta
+    m = m.replace("%f",fure)
+    m = m.replace("%w",who)
+    m = m.replace("%a",akira)
+    m = m.replace("%n",do)
+    m = m.replace("%d",place)
+    m = m.replace("%o",obj)
+    lst.append(m)
+    if i % prog == 0:
+      print('*',end='')
+    # print(m)
+  with open('aloh.txt', mode='w') as f:
+    f.write('\n'.join(lst))
+#   f.writelines(lst)
+
+#f a d n o
+#for i in range(10):
+#  print(ss, end='')
+
+buildrandstr(RNDSTRNUM)
